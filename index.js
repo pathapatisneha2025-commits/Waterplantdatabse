054fe00f9feb17f7aa818de89df75236955cd392
Sneha-datla <datla.sneha1@gmail.com>
2025-12-02 18:48:48 +0530

    changes made

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const Signup = require("./routes/signup");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
