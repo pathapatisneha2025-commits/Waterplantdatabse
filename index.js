0c638911b33785a17e64ea0a648c8d9758c83d4c
Sneha-datla <datla.sneha1@gmail.com>
2026-09-01 18:02:40 +0530

    changes made

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const Users = require("./routes/users");
 const UserCart = require("./routes/cart");
 const Orders = require("./routes/Orders");
@@ -13,28 +14,25 @@ const Banners = require("./routes/banner");
 const todaysdeals = require("./routes/todaysdeals");
 const wishlists = require("./routes/wishlists");
 
-
-
-
-
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// Use Router
+// Routes
 app.use("/users", Users);
 app.use("/cart", UserCart);
 app.use("/orders", Orders);
 app.use("/waterorder", WaterOrders);
-app.use("/groceries",Groceries);
-app.use("/categories",Categories);
-app.use("/banner",Banners);
-app.use("/todaydeals",todaysdeals);
-app.use("/wishlists",wishlists);
+app.use("/groceries", Groceries);
+app.use("/categories", Categories);
+app.use("/banner", Banners);
+app.use("/todaydeals", todaysdeals);
+app.use("/wishlists", wishlists);
 
+// Server
+const PORT = process.env.PORT || 5002;
 
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Waterplant server running on port ${PORT}`);
 });
