a3bf57e9682a1f434bb88cec559d160f09605106
Sneha-datla <datla.sneha1@gmail.com>
2026-09-01 15:51:35 +0530

    changes made

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const Users = require("./routes/users");
@@ -14,7 +17,6 @@ const wishlists = require("./routes/wishlists");
 
 
 
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
